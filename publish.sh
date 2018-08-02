5bd40bb11879265f6054e21528f7aeb94559729b
zhoushirong <1091263031@qq.com>
2018-08-02 21:15:39 +0800

    message

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo 'git add .; git commit -m "message"; git push;'
-git add .; git commit -m "message"; git push
 echo 'hexo clean && hexo g && hexo d:'
 hexo clean && hexo g && hexo d
+
+echo 'git add .; git commit -m "message"; git push;'
+git add .; git commit -m "message"; git push
 echo 'done!'
